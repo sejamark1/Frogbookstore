1455208ba5527ae96eff167276c82bc119f25e4b
acemark1 <jeyachandranajinath@gmail.com>
2022-12-08 23:52:13 +0000

    Initial commit

---
+++ src/main/java/com/example/frogbookstore/FrogbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.frogbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogbookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogbookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/frogbookstore/FrogbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frogbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogbookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
