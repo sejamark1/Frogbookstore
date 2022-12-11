c482c940242d5681743c27be5d4ab3e6035026ac
acemark1 <jeyachandranajinath@gmail.com>
2022-12-11 23:00:28 +0000

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,10 +12,4 @@ INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780450022852',
 INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780340837450', 'The Road to Dune', 'Frank Herbert');
 INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780450052620', 'God Emperor of Dune', 'Frank Herbert');
 
-INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('0-7475-3269-9', 'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling');
-INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780450058868', 'Chapterhouse: Dune', 'Frank Herbert');
-INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780450057779', 'Heretics of Dune', 'Frank Herbert');
-INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780450022852', 'Dune Messiah', 'Frank Herbert');
-INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780340837450', 'The Road to Dune', 'Frank Herbert');
-INSERT INTO BOOKSTORE (isbn_no, book_name, author_name) VALUES ('9780450052620', 'God Emperor of Dune', 'Frank Herbert');
 
